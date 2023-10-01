ca8f771b234c92aad6fdac7c7cd537f94b36c625
PalashGitCarscan <palash.g@carscan.co.za>
2023-10-01 18:05:39 +0530

    put api implementation

--- index.js
+++ index.js
@@ -1,90 +1,75 @@
 const express = require('express');
 const app = express();
+const bodyPareser = require('body-parser');
+app.use(bodyPareser.json());
 const mongoose = require('mongoose');
-const User = require('./users');
-const List = require('./list');
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
-mongoose.connect('mongodb+srv://palashghosh43442:ZFBHMQcNLrbc58jh@cluster0.7n22rqm.mongodb.net/tutorial?retryWrites=true&w=majority',
-{
+mongoose.connect('mongodb+srv://palashghosh43442:ZFBHMQcNLrbc58jh@cluster0.7n22rqm.mongodb.net/tutorial?retryWrites=true&w=majority',{
   useNewUrlParser: true,
   useUnifiedTopology: true
-}
-);
-
-var ObjectId = require('mongoose').Types.ObjectId;
-// List.find({}).then((list)=>{
-//   console.log('list is ', list);
-// }).catch((err)=>{
-//   console.error(err);
-// })
+});
+const User = require('./users');
 
 app.get('/users', (req, res)=>{
-  User.find()
-  .then((users) => {
-    res.status(201).json(users);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+User.find().then((users)=>{
+  res.status(200).json(users);
+}).catch((err)=>{
+  res.status(404).send('some error occured');
+})
 });
-app.post('/user', jsonParser, (req, res)=>{
+
+app.post('/users/user', (req, res)=>{
   const data = new User({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     email: req.body.email
+  });
+  data.save().then((result)=>{
+    res.status(201).json(result);
+  }).catch((err)=>{
+    res.status(500).send('Internal server error');
   })
-  if(req.body.name && req.body.email){
-    data.save().then((result)=>{
-      res.status(201).json(result);
-    })
-  }
-  else{
-    res.status(400).send('unexpected error occured');
-  }
-})
-
-// app.delete('/user/delete', jsonParser,(req, res)=>{
-//   User.deleteOne({_id: req.body.id}).then((result)=>{
-//     res.status(200).json(result);
-//   }).catch((err)=>{
-//     console.warn(err);
-//   })
-// })
-app.delete('/user/delete', jsonParser, (req, res) => {
-  User.deleteOne({ _id: req.body.id })
-    .then((result) => {
-      if (result.deletedCount === 1) {
-        res.status(200).json({ message: 'User deleted successfully' });
-      } else {
-        res.status(404).json({ message: 'User not found' });
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
-    });
 });
 
-  const data =  new User({
-    _id: mongoose.Schema.Types.ObjectId,
-    name: 'tony',
-    email: 'stark@gmail.com'
+app.delete('/users/delete', (req, res)=>{
+  User.deleteOne({ _id: req.body.id }).then((result)=>{
+              res.status(200).json({ message: 'User deleted successfully' });
+
+  }).catch((err)=>{
+    res.status(400).send('some error occuring');
   })
-  async function createUser() {
-    try {
-      const newUser = new User({
-        _id: new ObjectId(),
-        name: 'Palash Ghosh',
-        email: 'pgtest23@gmail.com'
-      });
+});
 
-      await newUser.save();
-      console.log('User saved successfully');
-    } catch (err) {
-      console.error('Error:', err);
-    }
-  }
+// app.delete('/users/delete', (req, res)=>{
+//   User.deleteOne({ _id: req.body.id })
+//   .then((result)=>{
+//     res.status(200).json({ succedd: "User deleted successfully" });
+//   })
+// })
 
-  // createUser();
-  app.listen(5000);
+// app.delete('/users/delete', (req, res) => {
+//   User.deleteOne({ _id: req.body.id })
+//     .then((result) => {
+//       if (result.deletedCount === 1) {
+//         res.status(200).json({ message: 'User deleted successfully' });
+//       } else {
+//         res.status(404).json({ message: 'User not found' });
+//       }
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).json({ message: 'Internal server error' });
+//     });
+// });
+
+app.put('/user/update',(req, res)=>{
+  User.updateOne({_id: req.body.id},
+    {$set: {
+      name: req.body.name,
+      email: req.body.email
+    }}
+    ).then((result)=>{
+      res.status(200).json(result);
+    })
+});
+
+app.listen(5000);
